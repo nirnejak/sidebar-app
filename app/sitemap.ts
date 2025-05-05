5e4e4271c59c17e840455575393e60ec1ff7894b
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-05-05 18:10:08 +0530

    Initial commit

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next"
+
+import config from "@/config"
+
+const { baseUrl } = config
+
+export const dynamic = "force-dynamic"
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const staticRoutes = [
+    {
+      url: baseUrl + `/`,
+      lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 1,
+    },
+    {
+      url: baseUrl + `/contact/`,
+      lastModified: new Date(),
+      changeFrequency: "weekly" as const,
+      priority: 0.8,
+    },
+  ]
+
+  return staticRoutes
+}
