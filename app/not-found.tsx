566e37ad8d5925dd839d124c39dec2bb3e1b1006
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-05-05 18:21:16 +0530

    Default colors updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,10 +15,10 @@ const NotFound: React.FC = () => {
     <main className="container">
       <section className="grid min-h-dvh place-content-center">
         <div className="text-center">
-          <h1 className="mb-3 text-5xl font-bold tracking-tighter text-zinc-800 dark:text-zinc-300">
+          <h1 className="mb-3 text-5xl font-bold tracking-tighter text-neutral-800 dark:text-neutral-300">
             404
           </h1>
-          <p className="text-base text-zinc-800 dark:text-zinc-300">
+          <p className="text-base text-neutral-800 dark:text-neutral-300">
             The page you&apos;re looking for cannot be found. Go{" "}
             <Link
               href="/"
